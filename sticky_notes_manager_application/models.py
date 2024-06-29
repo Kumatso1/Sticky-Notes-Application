fe441a14ba0e4fceb42921551ed9cd75921a0ccf
Dalison Chimchere <dchimchere2017@gmail.com>
2024-06-29 10:54:55 +0530

    Updating models.py and settings.py files

--- sticky_notes_manager_application/models.py
+++ sticky_notes_manager_application/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    is_admin = models.BooleanField(default=False)
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
